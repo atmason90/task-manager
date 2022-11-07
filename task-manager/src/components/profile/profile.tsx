cfa31c0cb41ad7f5ed999fa12275b649eda6c926
atmason90 <atmason90@gmail.com>
2022-11-07 13:37:51 -0800

    build layout for profile component

--- task-manager/src/components/profile/profile.tsx
+++ task-manager/src/components/profile/profile.tsx
@@ -4,14 +4,35 @@ import { Avatar, Box, Typography } from '@mui/material';
 
 export const Profile: FC = (): ReactElement => {
     {
-        // Avatar
-    }
-    {
         // Name
     }
     {
         // Welcome Message
     }
-    return (<h2>Profile</h2>)
+    return (
+        // container for profile
+        <Box
+            display='flex'
+            flexDirection='column'
+            justifyContent='center'
+            alignItems='center'
+        >
+            {/* Avatar from mui */}
+            <Avatar
+                sx={{
+                    width: '96px',
+                    height: '96px',
+                    backgroundColor: 'primary.main',
+                    marginBottom: '16px',
+                }}
+            >
+                <Typography variant='h4' color='text.primary'>J</Typography>
+            </Avatar>
+            {/* Name using typography from mui */}
+            <Typography variant='h6' color='text.primary'>Welcome, John</Typography>
+            {/* Welcome Message using typography from mui */}
+            <Typography variant='body1' color='text.primary'>This is your personal task manager</Typography>
+        </Box>
+    )
 }
 
