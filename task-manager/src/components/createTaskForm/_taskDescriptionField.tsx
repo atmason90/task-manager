0d0ab6e8d605810c9415f2ffa046c661065929b9
atmason90 <atmason90@gmail.com>
2022-11-07 22:37:29 -0800

    make createTaskForm component dynamic using interfaces

--- task-manager/src/components/createTaskForm/_taskDescriptionField.tsx
+++ task-manager/src/components/createTaskForm/_taskDescriptionField.tsx
@@ -1,16 +1,27 @@
 import React, { FC, ReactElement } from 'react';
 import { TextField } from '@mui/material';
+import { ITextField } from './interfaces/ITextField';
 
-export const TaskDescriptionField: FC = (): ReactElement => {
-  return <TextField
-  id='description'
-  label='Task Description'
-  placeholder='Task Description'
-  variant='outlined'
-  size='small'
-  name='title'
-  multiline
-  rows={4}
-  fullWidth
-  />
+export const TaskDescriptionField: FC<ITextField> = (props): ReactElement => {
+    // Destructure Props
+    const { 
+        onChange = (e) => console.log(e), 
+        disabled = false,
+    } = props;
+  
+    return (
+        <TextField
+            id='description'
+            label='Task Description'
+            placeholder='Task Description'
+            variant='outlined'
+            size='small'
+            name='title'
+            multiline
+            rows={4}
+            fullWidth
+            onChange={onChange}
+            disabled={disabled}
+        />
+  )
 };
--- task-manager/src/components/createTaskForm/_taskTitleField.tsx
+++ task-manager/src/components/createTaskForm/_taskTitleField.tsx
@@ -1,15 +1,27 @@
 import React, { FC, ReactElement } from 'react';
 import { TextField } from '@mui/material';
+import { ITextField } from './interfaces/ITextField';
 
-export const TaskTitleField: FC = (): ReactElement => {
-  return <TextField
-    id='title'
-    label='Task Title'
-    placeholder='Task Title'
-    variant='outlined'
-    size='small'
-    name='title'
-    fullWidth
-  />
+
+
+export const TaskTitleField: FC<ITextField> = (props): ReactElement => {
+    // Destructure props
+    const { 
+        onChange = (e) => console.log(e), 
+        disabled = false,
+    } = props;
+    return (
+    <TextField
+        id='title'
+        label='Task Title'
+        placeholder='Task Title'
+        variant='outlined'
+        size='small'
+        name='title'
+        fullWidth
+        disabled={disabled}
+        onChange={onChange}
+    />
+  )
 };
 
