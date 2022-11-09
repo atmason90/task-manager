0186464e4d31282e0a68a1144e51ba796ced889f
atmason90 <atmason90@gmail.com>
2022-11-08 21:47:10 -0800

    install dependencies and integrate/configure eslint and prettier

--- task-manager-api/index.ts
+++ task-manager-api/index.ts
@@ -1,10 +1,13 @@
 import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 // Instantiate express app
 const app: Express = express();
+dotenv.config();
 
 // Define server port
-const PORT = 3001;
+const PORT = process.env.PORT;
+
 
 
 // Create default route
