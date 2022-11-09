e06e16ffc9b18a946bde67057c18ec2bbb161ee9
atmason90 <atmason90@gmail.com>
2022-11-09 03:44:04 -0800

    implement update method

--- task-manager-api/src/tasks/tasks.controller.ts
+++ task-manager-api/src/tasks/tasks.controller.ts
@@ -1,8 +1,9 @@
 import { Task } from "./tasks.entity";
 import { AppDataSource } from "../../index";
-import { instanceToPlain } from 'class-transformer';
+import { instanceToPlain, plainToInstance } from 'class-transformer';
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
+import { UpdateResult } from 'typeorm';
 
 class TasksController {
     // Method for the get route
@@ -63,13 +64,50 @@ class TasksController {
     }
 
     // Method for put route
-    public async update(re: Request, res: Response): Promise<Response> {
+    public async update(req: Request, res: Response): Promise<Response> {
         const errors = validationResult(req);
         
         if(!errors.isEmpty()){
             return res
                 .status(400)
                 .json({ errors: errors.array() });
+        }
+
+        // Try to find the task and see if it exists
+        let task: Task | null;
+        try {
+            task = await AppDataSource.getRepository(Task).findOne({
+                where: { id: req.body.id },
+            })
+        } catch (errors) {
+            return res.json({error: 'Internal server error'}).status(500);
+        }
+
+        // Return 400 error if task is null
+        if (!task) {
+            return res.status(404).json({
+                error: 'The task with given ID does not exist'
+            })
+        }
+
+        // Declare a variable for updated task
+        let updatedTask: UpdateResult;
+
+        // Update the task
+        try {
+            updatedTask = await AppDataSource.getRepository(
+                Task
+            ).update(
+                req.body.id, 
+                plainToInstance(Task, {
+                    status: req.body.status
+                })
+            );
+             // Convert the updated task instance to an object
+            updatedTask = instanceToPlain(updatedTask) as UpdateResult;
+            return res.json(updatedTask).status(200);
+        } catch (errors) {
+            return res.json({error: 'Internal server error'}).status(500);
         }
     }
 }
