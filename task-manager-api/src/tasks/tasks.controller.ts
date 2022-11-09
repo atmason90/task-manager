000d20b3379a27473436c933c46f0408bffaedf0
atmason90 <atmason90@gmail.com>
2022-11-09 02:57:59 -0800

    move validation logic to the post controller

--- task-manager-api/src/tasks/tasks.controller.ts
+++ task-manager-api/src/tasks/tasks.controller.ts
@@ -2,9 +2,10 @@ import { Task } from "./tasks.entity";
 import { AppDataSource } from "../../index";
 import { instanceToPlain } from 'class-transformer';
 import { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
 
 class TasksController {
-    
+    // Method for the get route
     public async getAll(req: Request, res: Response): Promise<Response> {
         // Declare a variable to hold all tasks
         let allTasks: Task[];
@@ -26,6 +27,24 @@ class TasksController {
             return res.json({error: 'Internal server error'}).status(500);
         }
     }
+
+    // Method for the post route
+    public async create(req: Request, res: Response): Promise<Response> {
+        const errors = validationResult(req);
+        
+        if(!errors.isEmpty()){
+            return res
+                .status(400)
+                .json({ errors: errors.array() });
+        }
+
+        // Create a new instance of a task
+
+        // Add all the required properties of the task object
+
+        // save the newly created task to the database
+    }
+
 }
 
 export const taskController = new TasksController();
--- task-manager-api/src/tasks/tasks.router.ts
+++ task-manager-api/src/tasks/tasks.router.ts
@@ -1,7 +1,6 @@
 import  { Router, Request, Response } from 'express';
 import { taskController } from './tasks.controller';
 import { createValidator } from './tasks.validator';
-import { validationResult } from 'express-validator';
 
 // Fire the router function to create a new router
 export const tasksRouter: Router = Router();
@@ -10,14 +9,4 @@ export const tasksRouter: Router = Router();
 tasksRouter.get('/tasks', taskController.getAll);
 
 // Create a post route
-tasksRouter.post('/tasks', createValidator, 
-    // @ts-ignore
-    async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            return res
-                .status(400)
-                .json({ errors: errors.array() });
-        }
-    },
-);
+tasksRouter.post('/tasks', createValidator, taskController.create);
