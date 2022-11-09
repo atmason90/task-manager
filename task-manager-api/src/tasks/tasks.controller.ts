7e9e4b6ed3fc3fe5e272a3382efa9b8e504a7838
atmason90 <atmason90@gmail.com>
2022-11-09 03:24:26 -0800

    create put route

--- task-manager-api/src/tasks/tasks.controller.ts
+++ task-manager-api/src/tasks/tasks.controller.ts
@@ -63,7 +63,15 @@ class TasksController {
     }
 
     // Method for put route
-    
+    public async update(re: Request, res: Response): Promise<Response> {
+        const errors = validationResult(req);
+        
+        if(!errors.isEmpty()){
+            return res
+                .status(400)
+                .json({ errors: errors.array() });
+        }
+    }
 }
 
 export const taskController = new TasksController();
--- task-manager-api/src/tasks/tasks.router.ts
+++ task-manager-api/src/tasks/tasks.router.ts
@@ -1,6 +1,7 @@
 import  { Router } from 'express';
 import { taskController } from './tasks.controller';
-import { createValidator } from './tasks.validator';
+import { createValidator, updateValidator } from './tasks.validator';
+
 
 // Fire the router function to create a new router
 export const tasksRouter: Router = Router();
@@ -10,3 +11,6 @@ tasksRouter.get('/tasks', taskController.getAll);
 
 // Create a post route
 tasksRouter.post('/tasks', createValidator, taskController.create);
+
+// Create a put route
+tasksRouter.put('/tasks', updateValidator, taskController.update);
