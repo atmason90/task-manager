7649e08ab736574a6ec916ddd4d1a8a67ef689b2
atmason90 <atmason90@gmail.com>
2022-11-09 00:52:53 -0800

    create tasks controller and test route with httpYac

--- task-manager-api/src/tasks/tasks.controller.ts
+++ task-manager-api/src/tasks/tasks.controller.ts
@@ -1,5 +1,24 @@
 import { Task } from "./tasks.entity";
+import { AppDataSource } from "../../index";
 
 export class TaskController {
-    public async getAll(): Promise<Task[]>{}
+    constructor(private taskRepository = AppDataSource.getRepository(Task)) {}
+    
+    public async getAll(): Promise<Task[]> {
+        // Declare a variable to hold all tasks
+        let allTasks: Task[];
+        
+        // Fetch all tasks using the repository
+        try {
+            allTasks = await this.taskRepository.find({
+                order: {
+                    date: 'ASC',
+                }
+            });
+        } catch (errors){
+            console.log(errors)
+        }
+       
+        // Convert the tasks instance to an array of objects
+    }
 }
