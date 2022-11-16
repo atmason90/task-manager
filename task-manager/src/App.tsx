42fa9efe13db3d2682b8c9b5d818e47bd0194161
atmason90 <atmason90@gmail.com>
2022-11-16 00:01:21 -0800

    implement context in App.tsx

--- task-manager/src/App.tsx
+++ task-manager/src/App.tsx
@@ -4,6 +4,9 @@ import { customTheme } from './theme/customTheme';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import ComposeContext from './context/Compose.context';
+import { rootContext } from './context/root.context';
+
 
 // Create a query client
 const queryClient = new QueryClient();
@@ -11,10 +14,14 @@ const queryClient = new QueryClient();
 const App:FC = ():ReactElement => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={customTheme}>
-        <CssBaseline />
-        <Dashboard />
-      </ThemeProvider>
+      <ComposeContext
+        components={rootContext}
+      >
+        <ThemeProvider theme={customTheme}>
+          <CssBaseline />
+          <Dashboard />
+        </ThemeProvider>
+      </ComposeContext>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
